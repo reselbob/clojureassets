f8b5c3c7fa95cab864f69bbdc636e0d342e8c03a
reselbob <reselbob@gmail.com>
2022-01-02 15:31:32 -0800

    debugging

--- simple-json/project.clj
+++ simple-json/project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"] 
-                 [org.clojure/data.json "2.4.0"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.4.0"]
   :main ^:skip-aot simple-json.core
   :target-path "target/%s"
   :jvm-opts ["-Xmx1g"]
--- simple-json/src/simple_json/core.clj
+++ simple-json/src/simple_json/core.clj
@@ -1,11 +1,12 @@
 (ns simple-json.core
-  (require '[clojure.core])
-  (require '[clojure.string :as str])
-  (require '[clojure.data.json :as json])
+  (:require [clojure.string :as str]
+            [clojure.data.json :as json])
   (:gen-class))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (def all-records (clojure.data.json/read-str (slurp "/root/data/big-data.json")))
-  (println all-records))
+  (print all-records)
+  )
