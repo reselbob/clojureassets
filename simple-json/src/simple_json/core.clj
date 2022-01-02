6cee50da19bc9f836e66cab8d7e16aeed8b16d78
reselbob <reselbob@gmail.com>
2022-01-02 13:34:49 -0800

    adding simple-json

---
+++ simple-json/src/simple_json/core.clj
@@ -0,0 +1,10 @@
+(ns simple-json.core
+  (require '[clojure.core])
+  (require '[clojure.string :as str])
+  (require '[clojure.data.json :as json])
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
